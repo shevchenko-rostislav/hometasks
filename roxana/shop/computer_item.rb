5e40cea0a35a8bad211825d8226cb6ee0def55d6
Roxana <roxik1989@i.ua>
2015-05-12 02:51:08 +0300

    improve check 5 letters

--- roxana/shop/computer_item.rb
+++ roxana/shop/computer_item.rb
@@ -2,7 +2,7 @@ class ComputerItem
   attr_accessor :name_item, :price, :quantity, :category
 
   def initialize(name_item, price, quantity = 1, category = "other")
-    @name_item = name_item if name_item.size <= 5
+    name_item.size <= 5 ? @name_item = name_item : @name_item = name_item[0,5]
     @price = price
     if  @quantity != 1 then @quantity = quantity end
     if  @category != "other" then @category = category end
