8bdb65058152ac06169881db16ed5590413f2772
Roxana <roxik1989@i.ua>
2015-05-12 02:01:05 +0300

    new methods

--- roxana/shop/computer_item.rb
+++ roxana/shop/computer_item.rb
@@ -1,9 +1,10 @@
 class ComputerItem
-  attr_accessor :name_item, :price, :quantity
+  attr_accessor :name_item, :price, :quantity, :category
 
-  def initialize(name_item, price, quantity = 1)
-    @name_item = name_item
+  def initialize(name_item, price, quantity = 1, category = "other")
+    @name_item = name_item if name_item.size <= 5
     @price = price
     if  @quantity != 1 then @quantity = quantity end
+    if  @category != "other" then @category = category end
   end
 end
--- roxana/shop/shop.rb
+++ roxana/shop/shop.rb
@@ -1,5 +1,5 @@
 class Shop
-  attr_accessor :name_shop, :hash, :item, :items
+  attr_accessor :name_shop, :hash, :item, :items, :total_cost, :items_by_cat
 
   def initialize(name_shop)
     @name_shop = name_shop
@@ -11,12 +11,33 @@ class Shop
     @hash = {name_shop => @items}
   end
 
-  def view
-    puts "#{"Item name".center(15)}|#{"Item price".center(15)}|#{"Quantity".center(10)}"
-    @items.each{|item| puts "#{item.name_item.center(15)}|#{item.price.to_s.center(15)}|#{item.quantity.to_s.center(10)}"}
+  def batch_add(*item)
+    @items ||= []
+    @items += item
+    @hash = {@name_shop => @items}
   end
 
-  def remove(name)
-    @items.each {|item| if item.name_item == name then @items.delete(item) end}
+  def sort(order, order_type) #"name" or "price", into ascending order - asc, descending - "desc"
+    order = (order == "price") ? @items.sort_by{|item| item.price} : @items.sort_by{|item| item.name_item}
+    order_type == "desc" ? order.reverse : order
   end
+
+  def view(order, order_type)
+  	sort_items = sort(order, order_type)
+    puts "#{"Item name".center(15)}|#{"Item price".center(15)}|#{"Quantity".center(10)}"
+    sort_items.each{|item| puts "#{item.name_item.center(15)}|#{item.price.to_s.center(15)}|#{item.quantity.to_s.center(10)}"}
+  end
+
+  def total_cost
+  	@total_cost = 0
+    @items.each {|item| @total_cost += item.price}
+    puts "#{@total_cost}"
+  end
+
+  def select_cat(cat)
+    @items_by_cat = []
+    @items.each {|item| if (item.category == cat) then @items_by_cat << item end}
+    @items_by_cat
+  end
+
 end
