125757b65a5229de61fd7775010ce6932bb5076d
Roxana <roxik1989@i.ua>
2015-05-12 15:50:35 +0300

    new changes

--- roxana/shop/computer_item.rb
+++ roxana/shop/computer_item.rb
@@ -2,9 +2,9 @@ class ComputerItem
   attr_accessor :name_item, :price, :quantity, :category
 
   def initialize(name_item, price, quantity = 1, category = "other")
-    name_item.size <= 5 ? @name_item = name_item : @name_item = name_item[0,5]
+    @name_item = name_item.size <= 5 ? name_item : name_item[0,5]
     @price = price
-    if  @quantity != 1 then @quantity = quantity end
-    if  @category != "other" then @category = category end
+    @quantity = quantity
+    @category = category
   end
 end
--- roxana/shop/shop.rb
+++ roxana/shop/shop.rb
@@ -1,17 +1,19 @@
+require 'active_support'
+
 class Shop
-  attr_accessor :name_shop, :hash, :item, :items, :total_cost, :items_by_cat
+  attr_accessor :name_shop, :hash, :item, :items, :total_cost
 
   def initialize(name_shop)
     @name_shop = name_shop
-    @items ||= []
+    @items = []
   end
 
   def add(item)
     @items << item
   end
 
-  def batch_add(*item)
-    @items += item
+  def batch_add(*items)
+    @items += items
   end
 
   def sort(order, order_type) #"name" or "price", into ascending order - asc, descending - "desc"
@@ -20,21 +22,24 @@ class Shop
   end
 
   def view(order, order_type)
-  	sort_items = sort(order, order_type)
+    sort_items = sort(order, order_type)
     puts "#{"Item name".center(15)}|#{"Item price".center(15)}|#{"Quantity".center(10)}"
     sort_items.each{|item| puts "#{item.name_item.center(15)}|#{item.price.to_s.center(15)}|#{item.quantity.to_s.center(10)}"}
   end
 
+  def remove(name)
+    @items.delete(@items.find {|item| item.name_item == "aaaaa"})
+  end
+
+  def remove_qua(name_item, number)
+    @items.find{|item| item.name_item == name_item}.quantity -= number
+  end
+
   def total_cost
-  	@total_cost = 0
-    @items.each {|item| @total_cost += item.price}
-    puts "#{@total_cost}"
+    @items.inject(0){|sum, item| sum + item.price}
   end
 
   def select_cat(cat)
-    @items_by_cat = []
-    @items.each {|item| if (item.category == cat) then @items_by_cat << item end}
-    @items_by_cat
+    @items.find_all{|item| item.category == cat}
   end
-
 end
