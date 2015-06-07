9380d5d9be03c696e3131f19ac64c84701eaf240
Anna Khoma <annkhoma@gmail.com>
2015-06-07 12:05:51 +0300

    Devide code between three files

---
+++ anna/shop/item.rb
@@ -0,0 +1,9 @@
+class Item
+  attr_accessor :item_name, :price, :category
+
+  def initialize item_name, price
+    @item_name = item_name
+    @price = price
+    @category = category
+  end
+end
--- anna/shop/store.rb
+++ anna/shop/store.rb
@@ -1,56 +1,30 @@
 class Store
   attr_accessor :name, :list
 
-  def initialize name, list
+  def initialize(name, list)
     @name = name
     @list = list
   end
 end
 
-class Category 
-
-  attr_accessor :name, :item
-
-  def initialize name, item
-    @name = name
-    @item = item
-  end
-end
- 
-  def add_category_name name
-    @name << name
+  def add item
+    @items << item
   end
 
-class Item  
-  attr_accessor :item_name, :price
-
-  def initialize item_name, price
-    @item_name = item_name
-    @price = price
-  end
-end
-
-  def add_item item
-    @item << item
-  end
-
-  def show_item_by_name item_name
+  def show_item_by_name(item_name)
     item_to_show = [comp1, comp2, comp3]
     @item.each do |item|
-    if item.name == item_name 
+    if item.name == item_name
       item_to_show << item
+    end
+    end
   end
-end
-end
 
-  def show_item_by_price price
+  def show_item_by_price(price)
     item_to_show = [comp1, comp2, comp3]
     @item.each do |item|
     if item.price == price
       item_to_show << item
+    end
+    end
   end
-  end
-end 
-itemWidth = 10
-str = ' '
-puts str.ljust itemWidth
