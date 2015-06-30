dc612b6e8443256b36ce4eab0cc64c825ea81f05
Maxim Kachanov <m.b.kachanov@gmail.com>
2015-06-30 21:53:56 +0300

    implement listing items and random shop creation

--- maxim/shop/shop.rb
+++ maxim/shop/shop.rb
@@ -1,9 +1,20 @@
 class Shop
   attr_accessor :name, :items
 
-  def initialize(name)
+  def initialize(name, items = [])
     @name  = name
-    @items = []
+    @items = items
+  end
+
+  def self.random_shop
+    shop = Shop.new("Random shop")
+
+    5.times do |i|
+        i = Item.new("item-#{i+1}", rand(1000))
+        shop.add_item(i)
+    end
+
+    return shop
   end
 
   def info
@@ -12,5 +23,12 @@ class Shop
 
   def add_item(item)
     @items << item
+  end
+
+  def list_items
+    puts @name
+    @items.each do |item|
+      puts item.to_s
+    end
   end
 end
